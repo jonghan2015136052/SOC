af6529a2202ca40b734805b342240b97ae2946b4
jonghan2015136052 <71169265+jonghan2015136052@users.noreply.github.com>
2020-12-11 23:48:10 +0900

    dd
    ss

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
-from flask import Flask
+from flask import Flask,request, redirect, url_for, abort
 from flask import render_template
+import searchdata as d
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/index')
 def hello_world():
     return render_template('index.html')
+
 @app.route('/test')
 def htest():
     return render_template('test.html')
+
 @app.route('/landing')
 def hello_world3():
     return render_template('landing.html')
+
 @app.route('/generic')
 def hello_world4():
     return render_template('generic.html')
+
+
 @app.route('/result')
 def hello_world5():
-    return render_template('result.html')
+    return render_templsate('result.html')
+
+tmpli = []
+#사용자로부터 입력한 동네 읽어오는 거임 get방식으로
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method=='POST':
+        pass
+    elif request.method=='GET':
+        temp=request.args.get('demo-name')
+        tmpli = temp.split(",")
+        new_list = []
+        for v in tmpli:
+            if v not in new_list:
+                new_list.append(v)
+        score = 0
+        li = []
+        for i in new_list:
+            li.append((d.data1(i) + d.data2(i)) / 2)
+        li.sort(reverse=True)
+        print(li)
+        return (temp)
+    #arr=request.form.get('demo-name')
+    # print(arr)
+    # return arr
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -30,4 +63,5 @@ def show_post(post_id):
     return 'Post %d' % post_id
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = "5000")
+    app.run()#host = "0.0.0.0"
+
