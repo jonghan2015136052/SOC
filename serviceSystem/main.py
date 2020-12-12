076f141d9214712248ace5f49be90315f7602295
jonghan2015136052 <71169265+jonghan2015136052@users.noreply.github.com>
2020-12-12 16:10:49 +0900

    Update main.py

--- serviceSystem/main.py
+++ serviceSystem/main.py
@@ -1,7 +1,11 @@
 from flask import Flask,request, redirect, url_for, abort
 from flask import render_template
+import searchdata as d
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/index')
 def hello_world():
@@ -19,11 +23,12 @@ def hello_world3():
 def hello_world4():
     return render_template('generic.html')
 
+
 @app.route('/result')
 def hello_world5():
-    return render_template('result.html')
+    return render_templsate('result.html')
 
-
+tmpli = []
 #사용자로부터 입력한 동네 읽어오는 거임 get방식으로
 @app.route('/method', methods=['GET', 'POST'])
 def method():
@@ -31,7 +36,17 @@ def method():
         pass
     elif request.method=='GET':
         temp=request.args.get('demo-name')
-        print(temp)
+        tmpli = temp.split(",")
+        new_list = []
+        for v in tmpli:
+            if v not in new_list:
+                new_list.append(v)
+        score = 0
+        li = []
+        for i in new_list:
+            li.append((d.data1(i) + d.data2(i)) / 2)
+        li.sort(reverse=True)
+        print(li)
         return (temp)
     #arr=request.form.get('demo-name')
     # print(arr)
@@ -48,4 +63,5 @@ def show_post(post_id):
     return 'Post %d' % post_id
 
 if __name__ == '__main__':
-    app.run()#host = "0.0.0.0"
+    app.run(host = "0.0.0.0")#host = "0.0.0.0"
+
